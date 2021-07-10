cd3d2fbd4be9276eafc33ad34aeb0d7722507560
Grover_py <Nebu23z@yandex.ru>
2021-07-10 11:16:27 +0300

    ы

---
+++ task_polindrom.py
@@ -0,0 +1,13 @@
+a = [-1, -3, 10, 20, 21, 4, 3, 22, 10, -2, -15]
+
+a.sort()
+
+max_dif = 0
+
+for el in range(len(a) - 1):
+    x = abs(a[el]) - abs(a[el + 1])
+    if abs(x) > max_dif:
+        max_dif = abs(x)
+
+
+print(max_dif)
